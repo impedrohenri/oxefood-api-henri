34e15b70dd833e2a57d756cc64bfb1e1d6dcff03
Pedro H. Silva <impedrohenri@gmail.com>
2025-11-12 10:18:26 -0300

    adiciona rota de filtragem

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.categoriaProduto.CategoriaProdutoService;
@@ -20,8 +21,6 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
 
 @RestController
 @RequestMapping("/api/produto")
@@ -40,10 +39,10 @@ public class ProdutoController {
         produtoNovo.setCategoria(categoriaProdutoService.obterPorId(request.getIdCategoria()));
 
         Produto produto = produtoService.save(produtoNovo);
-        
+
         return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
     }
-    
+
     @GetMapping
     public List<Produto> listarTodos() {
         return produtoService.listarTodos();
@@ -61,14 +60,24 @@ public class ProdutoController {
         produto.setCategoria(categoriaProdutoService.obterPorId(request.getIdCategoria()));
 
         produtoService.update(id, produto);
-        
+
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         produtoService.delete(id);
 
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/filtrar")
+    public List<Produto> filtrar(
+            @RequestParam(value = "codigo", required = false) String codigo,
+            @RequestParam(value = "titulo", required = false) String titulo,
+            @RequestParam(value = "idCategoria", required = false) Long idCategoria) {
+
+        return produtoService.filtrar(codigo, titulo, idCategoria);
+    }
+
 }
